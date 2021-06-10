415ca9cdd9305e6aa736ae989f205acf06b8b40b
N3v3rKn0wn <74686888+N3v3rKn0wn@users.noreply.github.com>
2021-06-09 22:46:03 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,18 +3,20 @@ var mouse;
 var mouseimg;
 var targetimg;
 var targetimg, mouseimg;
+var score;
+score=0;
 function preload(){
   mouseimg=loadImage("image/optics.jpg");
   targetimg=loadImage("image/target.jpg") ;
 }
 function setup() {
-  createCanvas(1800,900);
+  createCanvas(2000,1000);
 
   mouse=createSprite(100,100,20,20);
   target=createSprite(700,120,100,100);
   target.x=Math.round(random(50,1750));
   target.y=Math.round(random(50,850));
-  mouse.shapeColor="red";
+  
 
   mouse.addImage(mouseimg);
   target.addImage(targetimg);
@@ -31,6 +33,8 @@ function draw() {
  text("Aim practice",700,300)
  textSize(50);
  text("click on the targets",700,500);
+ text("score:"+score,1600,100);
+ textSize(35);
 
  mouse.x=World.mouseX;
  mouse.y=World.mouseY;
@@ -43,6 +47,7 @@ function draw() {
     target.destroy;
     target.x=Math.round(random(50,1750));
     target.y=Math.round(random(50,850));
+    score=score+1;
   }
   
 }
